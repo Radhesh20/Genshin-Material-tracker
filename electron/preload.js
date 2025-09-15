0decbe3f8eb8527d3789ee865f2eedd90dbdae8a
RADHESH KUMAR <radheshkumar2004@gmail.com>
2025-09-15 18:54:09 +0530

    Updated package-lock.json

---
+++ electron/preload.js
@@ -0,0 +1,11 @@
+// Preload script for additional security
+// This file runs in the renderer process before the web content begins loading
+
+const { contextBridge } = require('electron');
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld('electronAPI', {
+  platform: process.platform,
+  versions: process.versions
+});
